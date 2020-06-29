bd5c0d1c80e9d8b4a685206cf9625a75b64c75c9
feiyangmeiyu <806567171@qq.com>
2020-06-29 15:10:56 +0200

    add the create product view

--- product/models.py
+++ product/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
-	title = models.CharField(max_length=200)
+	#title, url, pub_date
+	#votes_total, image, icon, body
+	title = models.CharField(max_length=225)
+	url = models.TextField(default="This is the url.")
+	pub_date = models.DateTimeField()
+	votes_total = models.IntegerField(default=1)
 	image = models.ImageField(upload_to='image/')
-	content = models.TextField(max_length=1000)
+	icon = models.ImageField(upload_to='image/')
+	body = models.TextField(default="This is product is amazing")
+	hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+	#if user is deleted, we also want to delete the product
+
+	def __str__(self):
+		return self.title
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
+
+	#hunter
+	
